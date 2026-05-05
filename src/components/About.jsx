ec3c252ec983db9fc5c63328f683b4adeb4c375d
St Michael Egenamba <michaelegenamba@gmail.com>
2026-05-05 09:09:02 -0700

    Added correct placeholder values

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const About = ({ image = "https://via.placeholder.com", about }) => {
+const About = ({ image = " https://via.placeholder.com/215", about }) => {
   return (
     <>
       <aside>
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Article = ({ title, date = "May 5 2026", preview }) => {
+const Article = ({ title, date = "January 1, 1970", preview }) => {
   return (
     <article>
       <h3>{title}</h3>
